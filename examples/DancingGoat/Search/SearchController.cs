ed3fc5ffa30b00c810de0d9cf06a13c19174744f
bkapustik <82807109+bkapustik@users.noreply.github.com>
2024-02-16 13:03:57 +0100

    polishing

--- examples/DancingGoat/Search/SearchController.cs
+++ examples/DancingGoat/Search/SearchController.cs
@@ -11,36 +11,36 @@ public class SearchController : Controller
 
     public SearchController(DancingGoatSearchService searchService) => this.searchService = searchService;
 
-    public async Task<IActionResult> Index(string query, int pageSize = 10, int page = 1, string indexName = null)
+    public async Task<IActionResult> Index(string? query, int? pageSize, int? page, string? indexName)
     {
-        var results = await searchService.GlobalSearch(indexName ?? "advanced", query, page, pageSize);
+        var results = await searchService.GlobalSearch(indexName ?? "advanced", query, page ?? 1, pageSize ?? 10);
         results.Endpoint = nameof(Index);
 
         return View(results);
     }
 
     [HttpGet(nameof(Geo))]
-    public async Task<IActionResult> Geo(string query, double latitude, double longitude, bool sortByDistance = true, int pageSize = 10, int page = 1, string indexName = null)
+    public async Task<IActionResult> Geo(string? query, double? latitude, double? longitude, bool? sortByDistance, int? pageSize, int? page, string? indexName)
     {
-        var results = await searchService.GeoSearch(indexName ?? "geo", query, latitude, longitude, sortByDistance, page, pageSize);
+        var results = await searchService.GeoSearch(indexName ?? "geo", query, latitude ?? 0, longitude ?? 0, sortByDistance ?? true, page ?? 1, pageSize ?? 10);
         results.Endpoint = nameof(Geo);
 
         return View("~/Views/Search/GeoSearch.cshtml", results);
     }
 
     [HttpGet(nameof(Simple))]
-    public async Task<IActionResult> Simple(string query, int pageSize = 10, int page = 1, string indexName = null)
+    public async Task<IActionResult> Simple(string? query, int? pageSize, int? page, string? indexName)
     {
-        var results = await searchService.SimpleSearch(indexName ?? "simple", query, page, pageSize);
+        var results = await searchService.SimpleSearch(indexName ?? "simple", query ?? "", page ?? 1, pageSize ?? 10);
         results.Endpoint = nameof(Simple);
 
         return View("~/Views/Search/Index.cshtml", results);
     }
 
     [HttpGet(nameof(Semantic))]
-    public async Task<IActionResult> Semantic(string query, int pageSize = 10, int page = 1, string indexName = null)
+    public async Task<IActionResult> Semantic(string? query, int? pageSize, int? page, string? indexName)
     {
-        var results = await searchService.SemanticSearch(indexName ?? "semantic", query, page, pageSize);
+        var results = await searchService.SemanticSearch(indexName ?? "semantic", query ?? "", page ?? 1, pageSize ?? 10);
         results.Endpoint = nameof(Semantic);
 
         return View("~/Views/Search/Index.cshtml", results);
