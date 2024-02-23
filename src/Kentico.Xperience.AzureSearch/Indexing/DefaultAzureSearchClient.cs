4bce6eaabe9d8a797413eda0afaf9dc9bb514b63
bkapustik <82807109+bkapustik@users.noreply.github.com>
2024-02-23 12:54:44 +0100

    Deleted warning

--- src/Kentico.Xperience.AzureSearch/Indexing/DefaultAzureSearchClient.cs
+++ src/Kentico.Xperience.AzureSearch/Indexing/DefaultAzureSearchClient.cs
@@ -22,7 +22,6 @@ internal class DefaultAzureSearchClient : IAzureSearchClient
     private readonly IInfoProvider<ChannelInfo> channelProvider;
     private readonly IConversionService conversionService;
     private readonly IProgressiveCache cache;
-    private readonly IEventLogService log;
     private readonly SearchIndexClient searchIndexClient;
 
     public DefaultAzureSearchClient(
@@ -33,7 +32,6 @@ internal class DefaultAzureSearchClient : IAzureSearchClient
         IInfoProvider<ChannelInfo> channelProvider,
         IConversionService conversionService,
         IProgressiveCache cache,
-        IEventLogService log,
         SearchIndexClient searchIndexClient)
     {
         this.azureSearchIndexClientService = azureSearchIndexClientService;
@@ -43,7 +41,6 @@ internal class DefaultAzureSearchClient : IAzureSearchClient
         this.channelProvider = channelProvider;
         this.conversionService = conversionService;
         this.cache = cache;
-        this.log = log;
         this.searchIndexClient = searchIndexClient;
     }
 
