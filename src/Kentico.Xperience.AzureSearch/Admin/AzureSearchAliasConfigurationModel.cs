5b878041dfe4c227cdd47b6675c881c293920028
michaljakubis <michalj@kentico.com>
2025-10-03 16:27:50 +0200

    Cleanup

--- src/Kentico.Xperience.AzureSearch/Admin/AzureSearchAliasConfigurationModel.cs
+++ src/Kentico.Xperience.AzureSearch/Admin/AzureSearchAliasConfigurationModel.cs
@@ -4,26 +4,49 @@ using Kentico.Xperience.Admin.Base.FormAnnotations;
 
 namespace Kentico.Xperience.AzureSearch.Admin;
 
+/// <summary>
+/// Represents the model for configuring an Azure Search index alias in the admin interface.
+/// </summary>
 public class AzureSearchAliasConfigurationModel
 {
+    /// <summary>
+    /// Identifier of the alias configuration.
+    /// </summary>
     public int Id { get; set; }
 
+
+    /// <summary>
+    /// Alias name for the Azure Search index.
+    /// </summary>
     [TextInputComponent(
         Label = "Alias Name",
         Order = 1)]
-    [Required]
-    [MinLength(1)]
+    [RequiredValidationRule]
     [MaxLength(128)]
     [RegularExpression("^(?!-)[a-z0-9-]+(?<!-)$", ErrorMessage = "Alias name must only contain lowercase letters, digits or dashes, cannot start or end with dashes and is limited to 128 characters.")]
     public string AliasName { get; set; } = string.Empty;
 
+
+    /// <summary>
+    /// Index name to which the alias will point.
+    /// </summary>
     [SingleGeneralSelectorComponent(dataProviderType: typeof(ExistingIndexOptionsProvider), Label = "Index Name", Order = 2)]
-    [Required(ErrorMessage = "You must select an index name")]
+    [RequiredValidationRule]
     public string IndexName { get; set; } = string.Empty;
 
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AzureSearchAliasConfigurationModel"/> class.
+    /// </summary>
     public AzureSearchAliasConfigurationModel()
     { }
 
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AzureSearchAliasConfigurationModel"/> class based on the provided alias information and index name.
+    /// </summary>
+    /// <param name="alias">The alias information.</param>
+    /// <param name="aliasIndex">The index name to which the alias will point.</param>
     public AzureSearchAliasConfigurationModel(
         AzureSearchIndexAliasItemInfo alias,
         string aliasIndex)
